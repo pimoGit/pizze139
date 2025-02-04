8c5587d46f89a5de6e8cbd537ebe430a76d1d14e
Simone <info@simoneicardi.com>
2025-02-04 12:40:33 +0100

    aggiunta rotta menu pizze

--- app.js
+++ app.js
@@ -10,6 +10,42 @@ app.get('/', (req, res) => {
     res.send("Ciao sono la rotta Home, della mia pizzeria!!!");
 })
 
+// definiamo la rotta menù della pizzeria
+app.get('/menu', (req, res) => {
+    // array di pizze da ritornare
+    const menu = [
+        {
+            id: 1,
+            name: "Margherita",
+            image: "http://localhost:3000/pizze/margherita.webp",
+            ingredients: ["pomodoro", "mozzarella"],
+        }, {
+            id: 2,
+            name: "Marinara",
+            image: "http://localhost:3000/pizze/marinara.jpeg",
+            ingredients: ["pomodoro", "aglio", "origano"],
+        }, {
+            id: 3,
+            name: "Diavola",
+            image: "http://localhost:3000/pizze/diavola.jpeg",
+            ingredients: ["pomodoro", "mozzarella", "salame piccante"],
+        }, {
+            id: 4,
+            name: "Bufalina",
+            image: "http://localhost:3000/pizze/bufalina.jpeg",
+            ingredients: ["pomodoro", "mozzarella di bufala"],
+        }, {
+            id: 5,
+            name: "4 formaggi",
+            image: "http://localhost:3000/pizze/4_formaggi.jpeg",
+            ingredients: ["pomodoro", "mozzarella", "gorgonzola", "parmigiano", "ricotta"],
+        }
+    ];
+
+    // ritorniamo l'array di pizze in formato Json per questo ednpoint dell'API
+    res.json(menu);
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
