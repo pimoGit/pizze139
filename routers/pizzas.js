22fb9bc5bf4afa8338800dac5ef40900c4f1603f
Simone <info@simoneicardi.com>
2025-02-06 10:35:51 +0100

    aggiunta filtro su rotta index

--- routers/pizzas.js
+++ routers/pizzas.js
@@ -10,7 +10,21 @@ const menu = require('../data/pizzas');
 // index
 router.get('/', function (req, res) {
     // res.send('Lista delle pizze');
-    res.json(menu);
+    // res.json(menu);
+
+    //Inizialmente, il menu filtrato corrisponde a quello originale
+    let filteredMenu = menu;
+
+    // Se la richiesta contiene un filtro, allora filtriamo il menu
+    if (req.query.ingredient) {
+        filteredMenu = menu.filter(
+            pizza => pizza.ingredients.includes(req.query.ingredient)
+        );
+    }
+
+    // restituiamo la variabile filteredMenu
+    // potrebbe essere stata filtrata o contenere il menu originale
+    res.json(filteredMenu);
 });
 // show
 router.get('/:id', function (req, res) {
